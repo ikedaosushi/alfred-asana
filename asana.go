765d5dcbf0799146b64fe48b286fe04eb07aefca
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-02-12 00:31:22 +0900

    Modify call api

--- asana.go
+++ asana.go
@@ -17,7 +17,7 @@ func envLoad() {
 	}
 }
 
-func createReq(url string) *http.Request {
+func get(url string) string {
 	accessToken := os.Getenv("ACCESS_TOKEN")
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -25,8 +25,9 @@ func createReq(url string) *http.Request {
 	}
 	auth := fmt.Sprintf("Bearer %s", accessToken)
 	req.Header.Set("Authorization", auth)
+	resp := request(req)
 
-	return req
+	return resp
 }
 
 func request(req *http.Request) string {
@@ -39,11 +40,31 @@ func request(req *http.Request) string {
 	return respStr
 }
 
+// Debug
 func main() {
 	envLoad()
-	url := "https://app.asana.com/api/1.0/users/me"
-	req := createReq(url)
-	resp := request(req)
-
+	var url, resp string
+	url = "https://app.asana.com/api/1.0/users/me"
+	resp = get(url)
 	fmt.Println(resp)
+
+	url = "https://app.asana.com/api/1.0/tasks?project=616529690850115&opt_fields=name,completed,due_on"
+	resp = get(url)
+	fmt.Println(resp)
+
+	// myurl := "https://app.asana.com/api/1.0/users/me"
+	// req := createReq(myurl)
+	// resp := request(req)
+	// fmt.Println(resp)
+
+	// myurl := "https://app.asana.com/api/1.0/tasks?limit=10"
+	// data := url.Values{}
+	// data.Set("project", "14641")
+	// r, _ := http.NewRequest("POST", myurl, bytes.NewBufferString(data.Encode()))
+
+	// accessToken := os.Getenv("ACCESS_TOKEN")
+	// auth := fmt.Sprintf("Bearer %s", accessToken)
+	// r.Header.Set("Authorization", auth)
+	// resp := request(r)
+
 }
